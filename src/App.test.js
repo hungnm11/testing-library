38f3417fcd97c5e7b7493ef95f75dec19cf67fd2
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2023-08-03 13:38:56 +0700

    test and code function

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { logRoles } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color', () => {
   const { container } = render(<App />);
   logRoles(container);
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
-    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
-    
-    fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 
-    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
-    expect(colorButton).toHaveTextContent('Change to red')
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton).toHaveTextContent('Change to red');
 });
 
-// test('button turns blue when clicked', () => {
-//     render(<App />);
-//     const colorButton = screen.getByRole('button', { name: 'Change to red' });
-//     expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
-// });
+describe('space before camel-case capital letter', () => {
+  test('Works for no inner capital letter', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+
+    test('Works for one inner capital letter', () => {
+        expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+});
